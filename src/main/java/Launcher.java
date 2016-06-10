b58cd5d5e1fd73ae1c17665cd224400b5bb95cab
Ashiq Mohamed <ashiqmoh@gmail.com>
2016-06-10 17:03:05 +0200

    Print out IPv4 address

--- src/main/java/Launcher.java
+++ src/main/java/Launcher.java
@@ -1,3 +1,10 @@
+import java.net.Inet4Address;
+import java.net.Inet6Address;
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.SocketException;
+import java.util.Enumeration;
+
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.servlet.ServletHandler;
 
@@ -9,6 +16,26 @@ public class Launcher {
 		server.setHandler(handler);
 		handler.addServletWithMapping("Map", "/");
 		server.start();
+		findLocalIp();
 		server.join();
+	}
+	
+	public static void findLocalIp() throws SocketException {
+		Enumeration<NetworkInterface> interfaces = NetworkInterface.getNetworkInterfaces();
+		while (interfaces.hasMoreElements())
+		{
+		    NetworkInterface current = interfaces.nextElement();
+		    if (!current.isUp() || current.isLoopback() || current.isVirtual()) continue;
+		    Enumeration<InetAddress> addresses = current.getInetAddresses();
+		    while (addresses.hasMoreElements())
+			{
+		        InetAddress current_addr = addresses.nextElement();
+		        if (current_addr.isLoopbackAddress()) continue;
+				if (current_addr instanceof Inet4Address)
+				{
+  					System.out.println("IPv4: "+current_addr.getHostAddress() + ":8080");
+				}
+		    }
+		}
 	}
 }
