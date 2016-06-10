062f693cf75ab9ea8becafc43950da6a9179889b
Ashiq Mohamed <ashiqmoh@gmail.com>
2016-06-10 17:26:34 +0200

    Print out IP address and google sheet ID

--- src/main/java/Launcher.java
+++ src/main/java/Launcher.java
@@ -1,10 +1,3 @@
-import java.net.Inet4Address;
-import java.net.Inet6Address;
-import java.net.InetAddress;
-import java.net.NetworkInterface;
-import java.net.SocketException;
-import java.util.Enumeration;
-
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.servlet.ServletHandler;
 
@@ -16,26 +9,8 @@ public class Launcher {
 		server.setHandler(handler);
 		handler.addServletWithMapping("Map", "/");
 		server.start();
-		findLocalIp();
+		System.out.println("Listening on >> 141.28.106.132:8080");
+		System.out.println("Google Sheets URL >> https://docs.google.com/spreadsheets/d/1kIdn6Eu2Q4QwsYIuPewgAVIW3o9ioEWnBcnwTkFsQpg/edit");
 		server.join();
-	}
-	
-	public static void findLocalIp() throws SocketException {
-		Enumeration<NetworkInterface> interfaces = NetworkInterface.getNetworkInterfaces();
-		while (interfaces.hasMoreElements())
-		{
-		    NetworkInterface current = interfaces.nextElement();
-		    if (!current.isUp() || current.isLoopback() || current.isVirtual()) continue;
-		    Enumeration<InetAddress> addresses = current.getInetAddresses();
-		    while (addresses.hasMoreElements())
-			{
-		        InetAddress current_addr = addresses.nextElement();
-		        if (current_addr.isLoopbackAddress()) continue;
-				if (current_addr instanceof Inet4Address)
-				{
-  					System.out.println("IPv4: "+current_addr.getHostAddress() + ":8080");
-				}
-		    }
-		}
 	}
 }
